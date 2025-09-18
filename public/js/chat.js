84812e99a527d59fc77667539556c5e57675100c
abhimango1998 <abhishek.kumar@mangoitsolutions.in>
2025-09-18 16:51:42 +0530

    added form elements states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,32 @@
 // We are initializing the io() on the client and here we also receive the socket just like in server so we can send req and res bidirectionally
 const socket = io();
 
+// DOM elements
+const $messageForm = document.querySelector("#message-form")
+const $location = document.querySelector("#location")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messageFormButton = $messageForm.querySelector("button")
+
+const $locationBtn = document.querySelector("#location")
+
+
 // receiving the event which server sends to us (client) 
 socket.on('message', (val) => {
     console.log(val)
 })
 
-document.querySelector("#message-form").addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const msg = e.target.elements.msgInput.value;
+
     socket.emit("sendMessage", msg, (err) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ""
+        $messageFormInput.focus()
+
         if (err) {
             return console.log(err)
         }
@@ -18,10 +35,13 @@ document.querySelector("#message-form").addEventListener('submit', (e) => {
     })
 });
 
-document.querySelector("#location").addEventListener("click", () => {
+$location.addEventListener("click", () => {
     if (!navigator.geolocation) {
         return alert("Geolocation not available")
     }
+
+    $locationBtn.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition(pos => {
         const data = {
             lat: pos.coords.latitude,
@@ -29,6 +49,7 @@ document.querySelector("#location").addEventListener("click", () => {
         }
 
         socket.emit("sendLocation", data, () => {
+            $locationBtn.removeAttribute("disabled")
             console.log("Location shared successfully")
         });
     })
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const path = require('path')
 const Filter = require('bad-words')
 
 const app = express()
-const server = http.createServer(app); //Passing app into http.createServer(app) wires up your Express app as the request handler for the HTTP server.
+// //Passing app into http.createServer(app) wires up your Express app as the request handler for the HTTP server.
+const server = http.createServer(app);
+
+//attaches Socket.IO to the same server so you can handle both HTTP routes and WebSocket events on the same port.
 const io = socketio(server)
 
 const publicDirPath = path.join(__dirname, '../public')
@@ -15,8 +18,6 @@ app.use(express.static(publicDirPath))
 io.on('connection', (socket) => {
     console.log('New web socket connection...')
 
-    // countUpdated event is use to send the initial count to the client and also to send any changes to the count.
-    // The second args is available on the client cb func param
     socket.emit("message", "Welcome")
     socket.broadcast.emit("message", "A new user has joined!");
 
